c40e36fa0ec604dc6e5f20da27a65a68eb661a5d
kentakaa <arshad9922ali@gmail.com>
2026-09-21 22:42:21 +0530

    fix

--- src/main/java/com/arshad/chat_backend/service/PresenceService.java
+++ src/main/java/com/arshad/chat_backend/service/PresenceService.java
@@ -8,12 +8,12 @@ public interface PresenceService {
 
     void setOnline(String userId, String sessionId);
     
-    // Jab user disconnect ho, use clean karo
+    
     void setOffline(String sessionId);
     
-    // Kisi specific user ka real-time status check karne ke liye
+    
     boolean isUserOnline(String userId);
     
-    // Ek group/room ke andar kaun-kaun se members online hain unhe filter karne ke liye
+    
     Set<String> filterOnlineUsers(Set<String> roomMembers);
 }
